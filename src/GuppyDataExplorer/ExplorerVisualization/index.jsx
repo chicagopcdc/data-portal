8fb523136bf818438f865a03f0a98a6af19a46c7
Spencer Claxton <saclaxton@gmail.com>
2024-07-08 16:41:09 -0700

    Add another missing change back

--- src/GuppyDataExplorer/ExplorerVisualization/index.jsx
+++ src/GuppyDataExplorer/ExplorerVisualization/index.jsx
@@ -65,7 +65,6 @@ function getChartData({
   nodeCountTitle,
   totalCount,
 }) {
-  console.log(aggsChartData);
   const summaries = [];
   const countItems = [{ label: nodeCountTitle, value: totalCount }];
   const stackedBarCharts = [];

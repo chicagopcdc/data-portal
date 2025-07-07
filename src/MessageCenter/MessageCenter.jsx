0c15da018b3f2d6abf45631b989a0a4054ffc7b6
Rachel Liu <hi.rachelliu@gmail.com>
2025-07-07 11:55:52 -0400

    Working message center with call to API endpoint

--- src/MessageCenter/MessageCenter.jsx
+++ src/MessageCenter/MessageCenter.jsx
@@ -1,60 +1,42 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './MessageCenter.css';
 
 function MessageCenter() {
-  const tableHeader = ['Subject', 'Date'];
-  const fullMessages = [
-    {
-      title: 'Routine website maintenance',
-      date: new Date('2025-06-22'),
-      message:
-        'Please be advised that there will be a scheduled maintenance on 6/18/25, 6 PM CT to 9 PM CT.',
-    },
-    {
-      title: 'Your updated login',
-      date: new Date('2025-06-23'),
-      message:
-        'You have updated your login! If you have not done so, please report this instance to support@gmail.com.',
-    },
-    {
-      title: 'New study available',
-      date: new Date('2025-06-27'),
-      message: 'A new study is available for viewing.',
-    },
-  ];
+  const tableHeader = ['ID', 'Date', 'Expiration Date'];
 
   // when msgIdx is null: no message is rendered
-  // else, renders the message from fullMessages[msgIdx].message
+  // else, renders the message with the index msgIdx
   const [msgIdx, toggleMsgIdx] = useState(null);
+  const [notifData, setNotifData] = useState([]);
 
   // renders and dismisses the message when clicking on message subject
-  
-  function handleMessageClick(tileIndex) {
-   toggleMsgIdx((prevMessageStatus) =>
-      prevMessageStatus === null ? toggleMsgIdx(tileIndex) : toggleMsgIdx(null),
+  function handleMessageClick(index) {
+    toggleMsgIdx((prevMessageStatus) =>
+      prevMessageStatus === null ? toggleMsgIdx(index) : toggleMsgIdx(null),
     );
   }
 
+  // fetches from API endpoint
   useEffect(() => {
-    fetch('/amanuensis/notifications/all ', {
+    fetch('/amanuensis/notifications/all', {
       method: 'GET',
-      credentials: 'include', 
+      credentials: 'include',
     })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error('Failed to fetch notifications');
-            }
-            return res.json();
-        })
-        .then(data => {
-          console.log('Notifications:', data);
-        })
-        .catch((error) => {
-            console.error("Fetch error:", error);
-        });
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch notifications');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setNotifData(data);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
   }, []);
 
-return (
+  return (
     <div>
       <h2>Your Message Center</h2>
       <table className='message_center_container'>
@@ -62,21 +44,29 @@ return (
           <tr>
             <th>{tableHeader[0]}</th>
             <th>{tableHeader[1]}</th>
+            <th>{tableHeader[2]}</th>
           </tr>
         </thead>
         <tbody>
-          {fullMessages.map((msg, index) => (
+          {notifData.map((msg, index) => (
             <React.Fragment key={index}>
-              <tr 
+              <tr
                 className='row-content'
                 onClick={() => handleMessageClick(index)}
               >
-                <td className='row-content' id='title'>{msg.title}</td>
-                <td className='row-content'>{msg.date.toLocaleDateString()}</td>
+                <td className='row-content' id='id'>
+                  {msg.id}
+                </td>
+                <td className='row-content'>
+                  {new Date(msg.create_date).toLocaleDateString()}
+                </td>
+                <td className='row-content'>
+                  {new Date(msg.expiration_date).toLocaleDateString()}
+                </td>
               </tr>
               {msgIdx === index && (
                 <tr>
-                  <td colSpan={2} className='message-box'>
+                  <td colSpan={tableHeader.length} className='message-box'>
                     {msg.message}
                   </td>
                 </tr>

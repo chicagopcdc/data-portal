732b804c8873e87dc975411f8d9aa888bda9afe5
Rachel Liu <hi.rachelliu@gmail.com>
2025-07-11 10:36:57 -0400

    Finished up banner

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -3,32 +3,45 @@ import { useState, useEffect } from 'react';
 
 export default function Banner() {
   const [showBanner, setShowBanner] = useState(true);
+  const [bannerData, setBannerData] = useState([]);
 
   function handleExitBanner() {
     setShowBanner(false);
   }
 
-  useEffect(()=> {
+  useEffect(() => {
     fetch('/amanuensis/notifications', {
       method: 'GET',
       credentials: 'include',
     })
-    .then((res)=> {
-      if (!res.ok) {
-        throw new Error('Failed to fetch notifications.')
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log("Notifications: ", data)
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-    })
-  }, []) 
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch notifications.');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log('Notifications: ', data);
+        setBannerData(data);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
 
-
-  const msg = 'THIS IS WHERE THE MESSAGE BANNER IS.';
+  // set the message shown on the banner to be the first one received
+  // from array given by API endpoint '/amanuensis/notifications' (unread notifications)
+  // if that array is empty, then no banner is shown
+  let msg;
+  function setMsg() {
+    if (bannerData.length === 0) {
+      msg = null;
+      setShowBanner(false);
+    } else {
+      msg = bannerData[0];
+    }
+  }
+  useEffect(setMsg, [bannerData]);
 
   return (
     <>

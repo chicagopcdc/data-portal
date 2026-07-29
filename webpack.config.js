d0dd6ab9fd13be8ec829ee779ad2273439d45fce
Sophia Wang <sophiawang@Sophias-MacBook-Air-2.local>
2026-07-29 17:00:59 -0400

    Allowed credentialed template requests in development

--- webpack.config.js
+++ webpack.config.js
@@ -145,7 +145,10 @@ module.exports = {
     port: 9443,
     https: true,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': devProxyTarget || 'https://localhost',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Accept, Content-Type, x-csrf-token',
+      'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
     },
   },
   module: {
